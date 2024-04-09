6c9040a0ae05ca57154879bdad7f99972a67055f
StructuredTrading <samowen1992@hotmail.com>
2024-04-09 20:46:47 +1000

    feat: Reconfigured, added files pokemonPrinter.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
+const pokemonPrinterFile = require("./pokemonPrinter");
+
+
+console.log(process.env.ENVIRONMENT_MESSAGE);
+
 console.log("terminal app is running!");
 
-const pokemon = require('pokemon');
+console.log(pokemonPrinterFile.pokemonPrinter());
 
-let randomPokemon = pokemon.random();
-console.log(`Today's lucky Pokemon is ${randomPokemon}`);
+// const pokemon = require('pokemon');
+
+// let randomPokemon = pokemon.random();
+// console.log(`Today's lucky Pokemon is ${randomPokemon}`);
 
 console.log("bye bye, terminal app is finished!");
