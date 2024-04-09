7c74c4b9f28fca009f84e4c4a64085370100f746
StructuredTrading <samowen1992@hotmail.com>
2024-04-09 19:56:21 +1000

    feat: Generate random pokemon and log it.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log("terminal app is running!");
 
+const pokemon = require('pokemon');
+
+let randomPokemon = pokemon.random();
+console.log(`Today's lucky Pokemon is ${randomPokemon}`);
+
 console.log("bye bye, terminal app is finished!");
